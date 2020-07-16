47e9bcb0131f640b9035e220cadafa81a446d18b
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-07-16 15:46:28 +0700

    add program class

--- src/main/java/com/codegym/classroom/controller/ApiController.java
+++ src/main/java/com/codegym/classroom/controller/ApiController.java
@@ -3,6 +3,7 @@ package com.codegym.classroom.controller;
 import com.codegym.classroom.model.Classes;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 public class ApiController {
 
     @Value("${apiUr}")
@@ -32,4 +34,5 @@ public class ApiController {
         List<Classes> classes = (List<Classes>) responseEntity.getBody();
         return new ResponseEntity<>(classes, HttpStatus.OK);
     }
+
 }
---
+++ src/main/java/com/codegym/classroom/model/Program.java
@@ -0,0 +1,10 @@
+package com.codegym.classroom.model;
+
+import lombok.Data;
+
+@Data
+public class Program {
+    private Long id;
+
+    private String name;
+}
