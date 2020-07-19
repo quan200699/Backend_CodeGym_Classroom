86d3dd124f8b421608a3d533e056500033a4c066
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-07-19 17:32:07 +0700

    get all program by mock api

--- src/main/java/com/codegym/classroom/controller/ApiController.java
+++ src/main/java/com/codegym/classroom/controller/ApiController.java
@@ -1,6 +1,7 @@
 package com.codegym.classroom.controller;
 
 import com.codegym.classroom.model.Classes;
+import com.codegym.classroom.model.Program;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -24,7 +25,7 @@ public class ApiController {
     private String principalRequestValue;
 
     @GetMapping("/classes")
-    public ResponseEntity<List<Classes>> show() {
+    public ResponseEntity<List<Classes>> showListClass() {
         final String uri = apiUrl + "/classes";
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
@@ -33,6 +34,19 @@ public class ApiController {
         ResponseEntity<Object> responseEntity = restTemplate.exchange(uri, HttpMethod.GET, entity, Object.class);
         List<Classes> classes = (List<Classes>) responseEntity.getBody();
         return new ResponseEntity<>(classes, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/programs")
+    public ResponseEntity<List<Program>> showListProgram() {
+        final String uri = apiUrl + "/programs";
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(principalRequestHeader, principalRequestValue);
+        HttpEntity<String> entity = new HttpEntity<>("headers", headers);
+        ResponseEntity<Object> responseEntity = restTemplate.exchange(uri, HttpMethod.GET, entity, Object.class);
+        List<Program> programs = (List<Program>) responseEntity.getBody();
+        return new ResponseEntity<>(programs, HttpStatus.OK);
     }
 
 }
