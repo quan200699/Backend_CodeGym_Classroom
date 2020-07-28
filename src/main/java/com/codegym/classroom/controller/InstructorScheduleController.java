9b02b36b7480345131281b57e2069209e6ee978c
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-07-28 13:40:53 +0700

    add instructor schedule controller

---
+++ src/main/java/com/codegym/classroom/controller/InstructorScheduleController.java
@@ -0,0 +1,53 @@
+package com.codegym.classroom.controller;
+
+import com.codegym.classroom.model.entity.CoachSchedule;
+import com.codegym.classroom.model.entity.InstructorSchedule;
+import com.codegym.classroom.service.instructor_schedule.IInstructorScheduleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/instructor-schedules")
+public class InstructorScheduleController {
+    @Autowired
+    private IInstructorScheduleService instructorScheduleService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<InstructorSchedule>> getAllInstructorSchedule() {
+        return new ResponseEntity<>(instructorScheduleService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<InstructorSchedule> getInstructorSchedule(@PathVariable Long id) {
+        Optional<InstructorSchedule> instructorScheduleOptional = instructorScheduleService.findById(id);
+        return instructorScheduleOptional.map(instructorSchedule -> new ResponseEntity<>(instructorSchedule, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<InstructorSchedule> createInstructorSchedule(@RequestBody InstructorSchedule instructorSchedule) {
+        return new ResponseEntity<>(instructorScheduleService.save(instructorSchedule), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<InstructorSchedule> updateInstructorSchedule(@PathVariable Long id, @RequestBody InstructorSchedule instructorSchedule) {
+        Optional<InstructorSchedule> instructorScheduleOptional = instructorScheduleService.findByClassId(id);
+        return instructorScheduleOptional.map(instructorSchedule1 -> {
+            instructorSchedule.setId(instructorSchedule1.getId());
+            return new ResponseEntity<>(instructorScheduleService.save(instructorSchedule), HttpStatus.OK);
+        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<InstructorSchedule> deleteInstructorSchedule(@PathVariable Long id) {
+        Optional<InstructorSchedule> instructorScheduleOptional = instructorScheduleService.findByClassId(id);
+        return instructorScheduleOptional.map(instructorSchedule -> {
+            instructorScheduleService.deleteByClassId(id);
+            return new ResponseEntity<>(instructorSchedule, HttpStatus.OK);
+        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+}
