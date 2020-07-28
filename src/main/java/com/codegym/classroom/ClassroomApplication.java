9ba496ecad50edf0ce3c4b9ed955432e815ac212
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-07-28 10:48:31 +0700

    add center FK to classroom

--- src/main/java/com/codegym/classroom/ClassroomApplication.java
+++ src/main/java/com/codegym/classroom/ClassroomApplication.java
@@ -24,22 +24,27 @@ public class ClassroomApplication {
             Classroom classroom1 = new Classroom();
             classroom1.setName("Tầng 3 TT01");
             classroom1.setCapacity(49);
+            classroom1.setCenterId(1L);
             classroomService.save(classroom1);
             Classroom classroom2 = new Classroom();
             classroom2.setName("Tầng 4 TT01");
             classroom2.setCapacity(45);
+            classroom2.setCenterId(1L);
             classroomService.save(classroom2);
             Classroom classroom3 = new Classroom();
             classroom3.setName("Tầng 1 TT04");
             classroom3.setCapacity(36);
+            classroom3.setCenterId(1L);
             classroomService.save(classroom3);
             Classroom classroom4 = new Classroom();
             classroom4.setName("Tầng 2 TT04");
             classroom4.setCapacity(30);
+            classroom4.setCenterId(1L);
             classroomService.save(classroom4);
             Classroom classroom5 = new Classroom();
             classroom5.setName("Tầng 3 TT04");
             classroom5.setCapacity(36);
+            classroom5.setCenterId(1L);
             classroomService.save(classroom5);
         }
     }
--- src/main/java/com/codegym/classroom/model/Classroom.java
+++ src/main/java/com/codegym/classroom/model/Classroom.java
@@ -3,7 +3,6 @@ package com.codegym.classroom.model;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.util.*;
 
 @Data
 @Entity
@@ -15,4 +14,6 @@ public class Classroom {
     private String name;
 
     private int capacity;
+
+    private Long centerId;
 }
